6df4292333f2ec0c30cc8aedafb1f2cae2378b1a
Zachary Dunn <zacharydunn@Zacharys-MBP.fios-router.home>
2019-05-20 14:56:14 -0400

    test5

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    var characters = {
+        "Obi-Wan Kenobi": {
+            name: "Obi-Wan Kenobi",
+            health: 120,
+            attack: 8,
+            imageUrl: "assets/images/obi-wan.jpg",
+            enemyAttackBack: 15
+        },
+        "Luke Skywalker": {
+            name: "Luke Skywalker",
+            health: 100,
+            attack: 14,
+            imageUrl: "assets/images/skywalker.jpg",
+            enemyAttackBack: 5
+        },
+        "Darth Sidious": {
+            name: "Darth Sidious",
+            health: 150,
+            attack: 8,
+            imageUrl: "assets/images/darth-sidious.jpg",
+            enemyAttackBack: 5
+        },
+        "Darth Maul": {
+            name: "Darth Maul",
+            health: 180,
+            attack: 7,
+            imageUrl: "assets/images/darth-maul.jpg",
+            enemyAttackBack: 25
+        }
+    
