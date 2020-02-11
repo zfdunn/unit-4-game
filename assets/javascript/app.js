e68b601a098590eef9df25d558e374d6ec68992e
Zach Dunn <zfdunn@comcast.net>
2020-02-11 16:32:47 -0500

    updated html, js, and css

---
+++ assets/javascript/app.js
@@ -0,0 +1,166 @@
+$(document).ready(function() {
+    var characters = {
+        'Obi-Wan Kenobi': {
+            name: 'Obi-Wan Kenobi',
+            health: 120,
+            attack: 8,
+            imageUrl: 'assets/images/obi-wan.jpg',
+            enemyAttackBack: 15,
+        },
+        'Luke Skywalker': {
+            name: 'Luke Skywalker',
+            health: 100,
+            attack: 14,
+            imageUrl: 'assets/images/skywalker.jpg',
+            enemyAttackBack: 5,
+        },
+        'Darth Sidious': {
+            name: 'Darth Sidious',
+            health: 150,
+            attack: 8,
+            imageUrl: 'assets/images/darth-sidious.jpg',
+            enemyAttackBack: 5,
+        },
+        'Darth Maul': {
+            name: 'Darth Maul',
+            health: 180,
+            attack: 7,
+            imageUrl: 'assets/images/darth-maul.jpg',
+            enemyAttackBack: 25,
+        },
+    };
+
+    var attacker;
+    var combatants = [];
+    var defender;
+    var turnCounter = 1;
+    var killCount = 0;
+
+    var renderCharacter = function(character, renderArea) {
+        var charDiv = $(
+            "<div class='character' data-name'" + character.name + "'>",
+        );
+        var charName = $("<div class = 'character-name'>").text(character.name);
+        var charImage = $("<img alt='image' class='character-image'>").attr(
+            'src',
+            character.imageUrl,
+        );
+        var charHealth = $("<div class='character-health'>").text(
+            character.health,
+        );
+        charDiv
+            .append(charName)
+            .append(charImage)
+            .append(charHealth);
+        $(renderArea).append(charDiv);
+    };
+
+    var intializeGame = function() {
+        for (var key in characters) {
+            renderCharacter(characters[key], '#characters-section');
+        }
+    };
+    intializeGame();
+
+    var updateCharacter = function(charObj, areaRender) {
+        $(areaRender).empty();
+        renderCharacter(charObj, areaRender);
+    };
+
+    var renderEnemies = function(enemyArr) {
+        for (var i = 0; i < enemyArr.length; i++) {
+            renderCharacter(enemyArr[i], '#available-to-attack-section');
+        }
+    };
+
+    var renderMessage = function(messge) {
+        var gameMessageSet = $('#game-messge');
+        var newMessage = $('<div>').text(message);
+        gameMessageSet.append(newMessage);
+    };
+
+    var restartGame = function(resultMessage) {
+        var restart = $('<button>Restart</button>').click(function() {
+            location.reload();
+        });
+        var gameState = $('<div>').text(resultMessage);
+        $('body').append(gameState);
+        $('body').append(restart);
+    };
+
+    var clearMessage = function() {
+        var gameMessage = $('#game-message');
+        gameMessage.text('');
+    };
+    $('#characters-section').on('click', '.character', function() {
+        var name = $(this).attr('data-name');
+        if (!attacker) {
+            attacker = characters[name];
+            for (var key in characters) {
+                if (key !== name) {
+                    combatants.push(characters[key]);
+                }
+            }
+            $('#characters-section').hide();
+            updateCharacter(attacker, '#selected-character');
+            renderEnemies(combatants);
+        }
+    });
+
+    $('#available-to-attack-section').on('click', '.character', function() {
+        var name = $(this).attr('data-name');
+        if ($('#defender').children().length === 0) {
+            defender = characters[name];
+            updateCharacter(defender, '#defender');
+            $(this).remove();
+            clearMessage();
+        }
+    });
+
+    $('#attack-button').on('click', function() {
+        if ($('#defender').children().length !== 0) {
+            var attackMessage =
+                'You attacked ' +
+                defender.name +
+                ' for ' +
+                attacker.attack * turnCounter +
+                ' damage.';
+            var counterAttackMessage =
+                defender.name +
+                ' attacked you back for ' +
+                defender.enemyAttackBack +
+                ' damage.';
+            clearMessage();
+            defender.health -= attacker.attack * turnCounter;
+            if (defender.health > 0) {
+                updateCharacter(defender, '#defender');
+                renderMessage(attackMessage);
+                renderMessage(counterAttackMessage);
+                attacker.health -= defender.enemyAttackBack;
+                updateCharacter(attacker, '#selected-character');
+                if (attack.health <= 0) {
+                    clearMessage();
+                    restartGame('You have been defeated...GAME OVER!');
+                    $('#attack-button').off('click');
+                }
+            } else {
+                $('defender').empty();
+                var gameSateMessage =
+                    'You have defeated ' +
+                    defender.name +
+                    ', you can choose to fight another enemy, should you dare!';
+                renderMessage(gameSateMessage);
+                killCount++;
+                if (killCount >= combatants.length) {
+                    clearMessage();
+                    $('#attack-button').off('click');
+                    restartGame('You Won!!! GAME OVER!');
+                }
+            }
+            turnCounter++;
+        } else {
+            clearMessage();
+            renderMessage('No enemies here.');
+        }
+    });
+});
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,79 @@
 $(document).ready(function() {
     var characters = {
-        "Obi-Wan Kenobi": {
-            name: "Obi-Wan Kenobi",
+        'Obi-Wan Kenobi': {
+            name: 'Obi-Wan Kenobi',
             health: 120,
             attack: 8,
-            imageUrl: "assets/images/obi-wan.jpg",
-            enemyAttackBack: 15
+            imageUrl: 'assets/images/obi-wan.jpg',
+            enemyAttackBack: 15,
         },
-        "Luke Skywalker": {
-            name: "Luke Skywalker",
+        'Luke Skywalker': {
+            name: 'Luke Skywalker',
             health: 100,
             attack: 14,
-            imageUrl: "assets/images/skywalker.jpg",
-            enemyAttackBack: 5
+            imageUrl: 'assets/images/skywalker.jpg',
+            enemyAttackBack: 5,
         },
-        "Darth Sidious": {
-            name: "Darth Sidious",
+        'Darth Sidious': {
+            name: 'Darth Sidious',
             health: 150,
             attack: 8,
-            imageUrl: "assets/images/darth-sidious.jpg",
-            enemyAttackBack: 5
+            imageUrl: 'assets/images/darth-sidious.jpg',
+            enemyAttackBack: 5,
         },
-        "Darth Maul": {
-            name: "Darth Maul",
+        'Darth Maul': {
+            name: 'Darth Maul',
             health: 180,
             attack: 7,
-            imageUrl: "assets/images/darth-maul.jpg",
-            enemyAttackBack: 25
-        }
+            imageUrl: 'assets/images/darth-maul.jpg',
+            enemyAttackBack: 25,
+        },
     };
 
     var currSelectedCharacter;
 
-        // console.log(characters);
-// functions below
-        var renderOne = function(character, renderArea){
-            var charDiv = $("div class = 'character' data-name'" + character.name + "'>");
-            var charName = $("<div class='character.name'>").text(character.name);
-            var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
-            var charHealth = $("<div class='character-health'>").text(character_health);
-            charDiv.append(charName).append(charImage).append(charHealth);
-            $(renderArea).append(charDiv);
+    // console.log(characters);
+    // functions below
+    var renderOne = function(character, renderArea) {
+        var charDiv = $(
+            "div class = 'character' data-name'" + character.name + "'>",
+        );
+        var charName = $("<div class='character.name'>").text(character.name);
+        var charImage = $("<img alt='image' class='character-image'>").attr(
+            'src',
+            character.imageUrl,
+        );
+        var charHealth = $("<div class='character-health'>").text(
+            character_health,
+        );
+        charDiv
+            .append(charName)
+            .append(charImage)
+            .append(charHealth);
+        $(renderArea).append(charDiv);
+    };
 
-        }
-
-        var renderCharacters = function(charObj, autoRenders) {
-            if (areaRender === "#characters-section") {
-                $(areaRender).empty()
-                for (var key in charObj) {
-                    if(charObj.hasOwnProperty(key)) {
-                        renderOne(charObj[key], areaRender);
+    var renderCharacters = function(charObj, autoRenders) {
+        if (areaRender === '#characters-section') {
+            $(areaRender).empty();
+            for (var key in charObj) {
+                if (charObj.hasOwnProperty(key)) {
+                    renderOne(charObj[key], areaRender);
                 }
             }
         }
         // on click event
-        renderCharacters(characters, "#characters-sections");
-        $(document).on("click", ".character", function() {
-            var name = $(this).attr("data-name");    
+        renderCharacters(characters, '#characters-sections');
+        $(document).on('click', '.character', function() {
+            var name = $(this).attr('data-name');
             console.log(name);
-        })
-        if(!currSelectedCharacter) {
+        });
+        if (!currSelectedCharacter) {
             for (var key in characters) {
                 if (key !== name) {
                     combatants.push(characters[key]);
                 }
             }
         }
-    }
+    };
 });
